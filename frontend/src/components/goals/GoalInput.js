1e8215819a23301eaac5d5cda165497c84162169
ayusmitadutta <sati.v1011@gmail.com>
2023-09-03 12:38:22 +0530

    docker-compose setup and run

--- frontend/src/components/goals/GoalInput.js
+++ frontend/src/components/goals/GoalInput.js
@@ -27,7 +27,7 @@ function GoalInput(props) {
     <section id='goal-input'>
       <Card>
         <form onSubmit={goalSubmitHandler}>
-          <label htmlFor='text'>Add Your New Goal</label>
+          <label htmlFor='text'>Add Your New Goal...</label>
           <input
             type='text'
             id='text'
